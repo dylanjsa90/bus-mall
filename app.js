184de6e3f42f5f0c2ef4c5d1390498f39f4e8e5d
dylanjsa90 <dylanjsanders1@gmail.com>
2016-05-24 12:45:10 -0700

    graph added

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ var totalClicks = 0;
 var left = document.getElementById('left');
 var center = document.getElementById('center');
 var right = document.getElementById('right');
+var nameChartData = [];
+var clicksChartData = [];
+// var graphDrawn = false;
+var barData = {
+  labels: nameChartData,
+  dataSets: [
+    {
+      data: clicksChartData
+    } ]
+};
 
 var productArray = [];
 var container = document.getElementById('container');
 
-function Product(productName, path) {
+function Product(productName) {
   this.productName = productName;
-  this.path = 'img/' + path + '.jpg';
+  this.path = 'img/' + productName + '.jpg';
   this.clicks = 0;
   this.displayed = 0;
   productArray.push(this);
@@ -27,14 +37,41 @@ function handleProductClick(event) {
       displayProducts();
       totalClicks += 1;
       // var imgID = event.target.id;
-      var selected = document.getElementById(event.target.id);
       for (var i; i < productArray.length; i++) {
-        if (selected.src === productArray[i].path) {
+        if (event.target.alt === productArray[i].name) {
           productArray[i].clicks += 1;
         }
       }
+    } else {
+      buildNameData();
+      displayGraph();
     }
   }
+}
+
+function handleButtonClick(event) {
+  updateChartData();
+  drawGraph();
+}
+
+function updateChartData() {
+  for (var i = 0; i < productArray.length; i++) {
+    nameChartData[i] = productArray[i];
+    clicksChartData[i] = productArray[i];
+  }
+}
+
+function drawGraph() {
+  var bar = document.getElementById('graph').getContext('2d');
+  barGraph = new Chart(bar, {
+    type: 'bar',
+    data: barData,
+    options: {
+      reponsive: false,
+    }
+  });
+
+  // graphDrawn = true;
 }
 
 function displayProducts() {
@@ -52,7 +89,6 @@ function displayProducts() {
   center.src = productArray[centerIndex].path;
   center.alt = productArray[centerIndex].name;
 
-
   var rightIndex = randIndex(0, productArray.length);
   while (rightIndex === leftIndex || rightIndex === centerIndex) {
     rightIndex = randIndex(0, productArray.length);
@@ -61,7 +97,6 @@ function displayProducts() {
   right.src = productArray[rightIndex].path;
   right.alt = productArray[rightIndex].name;
 
-
 }
 
 for (var i = 0; i < 20; i++) {
@@ -69,4 +104,5 @@ for (var i = 0; i < 20; i++) {
 }
 
 container.addEventListener('click', handleProductClick);
+document.getElementById('results').addEventListener('click', handleButtonClick);
 displayProducts();
